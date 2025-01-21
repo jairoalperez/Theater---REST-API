0e48577f23529d00f2e15f9a56b475044c22014f
Jairoalperez <jairoalberto98@hotmail.com>
2025-01-21 14:05:27 -0500

    database migrations done

---
+++ Data/Queries.sql
@@ -0,0 +1,10 @@
+-- Create Database
+CREATE DATABASE Theater;
+
+-- Use Database
+USE ToDoList;
+GO
+
+-- ON PROJECT TERMINAL EXECUTE NEXT TWO COMMANDS
+-- -- dotnet ef migrations add InitialCreate
+-- -- dotnet ef database update

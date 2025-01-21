73d4d95395e2aa376d9af111af4490e88aae8ae1
Jairoalperez <jairoalberto98@hotmail.com>
2025-01-21 13:11:36 -0500

    added relation to Reference

--- Models/Reference.cs
+++ Models/Reference.cs
@@ -3,6 +3,7 @@ namespace Actors_RestAPI.Models
     public class Reference
     {
         public int ReferenceId { get; set; }
+        public int PlayId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
@@ -11,5 +12,7 @@ namespace Actors_RestAPI.Models
         public string Genre { get; set; } = string.Empty;
         public string? Image { get; set; }
         public string? Link { get; set; }
+
+        public required Play Play { get; set; }
     }
 }

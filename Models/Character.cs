3332198ec067bba26b72342584354622e0981491
Jairoalperez <jairoalberto98@hotmail.com>
2025-01-21 12:38:06 -0500

    created character model

---
+++ Models/Character.cs
@@ -0,0 +1,13 @@
+namespace Actors_RestAPI.Models
+{
+    public class Character
+    {
+        public int CharacterId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string? Image { get; set; }
+        public int ActorId { get; set; }
+        public int PlayId { get; set; }
+    }
+}

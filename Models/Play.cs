eb0d5c1c57f4fdfb1bc3fcb7a58cd7e283b39a7d
Jairoalperez <jairoalberto98@hotmail.com>
2025-01-21 12:26:36 -0500

    created play model

---
+++ Models/Play.cs
@@ -0,0 +1,12 @@
+namespace Actors_RestAPI.Models
+{
+    public class Play
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Format { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string? Poster { get; set; }
+    }
+}

8dbf264e0ae5c7f2462eb895c85fb66fec7332d8
Jairoalperez <jairoalberto98@hotmail.com>
2025-01-22 14:38:48 -0500

    created endpoint to get character data

--- Controllers/CharactersController.cs
+++ Controllers/CharactersController.cs
@@ -17,5 +17,55 @@ public class CharactersController : ControllerBase
         _context = context;
     }
 
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        try
+        {
+            var character = await _context.Characters
+            .Where(c => c.CharacterId == id)
+            .Include(c => c.Actor)
+            .Include(c => c.Play)
+            .Select(c => new
+            {
+                c.CharacterId,
+                c.Name,
+                c.Description,
+                c.Age,
+                c.Gender,
+                c.Principal,
+                c.Image,
+                Actor = new 
+                {
+                    c.Actor.ActorId,
+                    c.Actor.FirstName,
+                    c.Actor.LastName,
+                    c.Actor.Gender,
+                    c.Actor.FrontImage
+                },
+                Play = new
+                {
+                    c.Play.PlayId,
+                    c.Play.Title,
+                    c.Play.Genre,
+                    c.Play.Format,
+                    c.Play.Poster
+                }
+            })
+            .FirstOrDefaultAsync();
+
+            if (character == null)
+            {
+                return NotFound(Messages.Characters.NotFound);
+            }
+
+            return Ok(character);
+        }
+        catch (Exception ex)
+        {
+            return Problem(Messages.Database.ProblemRelated, ex.Message);
+        }
+    }
+
     
 }

544f1590d70a269d4120e3e651a5263cf340b012
jairoalperez <jairoalperezprof@gmail.com>
2025-07-23 15:43:16 -0400

    added swagger to all environments

--- Program.cs
+++ Program.cs
@@ -39,14 +39,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+
 app.MapControllers();
 
 app.UseCors("AllowAll");
